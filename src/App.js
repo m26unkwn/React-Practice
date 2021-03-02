e3114c8a3b233a71cde9df562cb1b8b9783771cd
m26unkwn <shukla9915294376@gmail.com>
2021-03-02 23:41:44 +0530

    pracice

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import { Component } from "react";
-
+import { useState } from "react";
 import "./App.css";
+const App = () => {
+  const [todo, setTodo] = useState([]);
+  const [Input, setInput] = useState("");
 
-import { Parent } from "./components/parentToChild/Parent";
-
-import Users from "./users/Users";
-
-class App extends Component {
-  state = {
-    title: "placeholder title",
-  };
-  doSomething = (newTitle) => {
-    this.setState({ title: newTitle });
+  const addList = (e) => {
+    e.preventDefault();
+    setTodo([...todo, Input]);
+    setInput("");
   };
 
-  render() {
-    return (
-      <div className='app'>
-        <Parent
-          doSomething={this.doSomething.bind(this, "ParentToChild")}
-          title={this.state.title}
-        />
-        <Users />
+  const deleteList = (index, e) => {
+    const todoList = Object.assign([], todo);
+    todoList.splice(index, 1);
+    setTodo([todoList]);
+  };
+
+  return (
+    <>
+      <input
+        type='text'
+        value={Input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addList}>Add</button>
+      <div>
+        {todo.map((list, key) => (
+          <>
+            <div class='todoList' key={key}>
+              <span>
+                {list} <input type='checkbox' />
+                <button onClick={deleteList}>delete</button>
+              </span>
+            </div>
+          </>
+        ))}
       </div>
-    );
-  }
-}
+    </>
+  );
+};
+
 export default App;
--- src/components/parentToChild/Parent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {Child} from "./Child";
-export const Parent = (props) => {
-    return (
-        <div>
-            <Child {...props}/>
-        </div>
-    )
-}
