f09ec0e381c32ca167adb6ce852b3db9d2b51240
m26unkwn <shukla9915294376@gmail.com>
2021-03-03 19:58:57 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
-import { useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "./App.css";
 
+const API = "https://hn.algolia.com/api/v1/search?query=";
+const DEFAULT_QUERY = "redux";
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       title: "Hello!",
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch(API + DEFAULT_QUERY)
+//       .then((response) => response.json())
+//       .then((data) => this.setState({ title: data.hits[0].title }));
+//   }
+
+//   render() {
+//     return <h1> {this.state.title} </h1>;
+//   }
+// }
 const App = () => {
-  const [data, setData] = useState(0);
-  const oIncrement = (e) => {
-    setData((prev) => {
-      if (prev > 0) {
-        return prev * 2;
-      } else {
-        return prev + 1;
-      }
-    });
-  };
-  const oDecrement = (e) => {
-    setData((prev) => {
-      if (prev > 0) {
-        return prev - 10;
-      } else {
-        return prev;
-      }
-    });
-  };
+  const [data, setData] = useState({ title: "hello" });
+
+  useEffect(() => {
+    const result = async () => {
+      let result = await fetch(API + DEFAULT_QUERY);
+      let res = await result.json();
+      console.log(res);
+      setData({ title: res.hits[1].title });
+    };
+    result();
+  }, []);
 
   return (
     <div>
-      <button onClick={oIncrement} style={{ margin: "2rem", color: "green" }}>
-        Increment
-      </button>
-
-      <span>{data}</span>
-
-      <button onClick={oDecrement} style={{ margin: "2rem", color: "red" }}>
-        Decrement
-      </button>
+      <h1>{data.title}</h1>
     </div>
   );
 };
